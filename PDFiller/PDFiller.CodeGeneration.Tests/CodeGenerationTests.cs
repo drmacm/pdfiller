6e9f17d43406a762981ca6ed9c8f6582c6c8581b
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-01 07:55:44 +0200

    Exploring Roslyn API

--- PDFiller/PDFiller.CodeGeneration.Tests/CodeGenerationTests.cs
+++ PDFiller/PDFiller.CodeGeneration.Tests/CodeGenerationTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace PDFiller.CodeGeneration.Tests
@@ -8,13 +10,29 @@ namespace PDFiller.CodeGeneration.Tests
     public class CodeGenerationTests
     {
         [Fact]
-        public async void Test1()
+        public async void CanFindFooProperty()
         {
             var fileName = @"SampleCodeFiles\SamplePoco.cs";
 
             var code = await File.ReadAllTextAsync(fileName);
 
             var tree = CSharpSyntaxTree.ParseText(code);
+
+            var syntaxRoot = tree.GetRoot();
+
+            var fooProperty = syntaxRoot
+                .DescendantNodes()
+                .OfType<PropertyDeclarationSyntax>()
+                .Where(p => p.Identifier.ValueText == "Foo")
+                .FirstOrDefault();
+
+            var barProperty = syntaxRoot
+                .DescendantNodes()
+                .OfType<PropertyDeclarationSyntax>()
+                .Where(p => p.Identifier.ValueText == "Bar")
+                .FirstOrDefault();
+            Assert.NotNull(fooProperty);
+            Assert.Null(barProperty);
         }
     }
 }
