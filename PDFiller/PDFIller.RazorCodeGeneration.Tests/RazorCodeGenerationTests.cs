3d7cedd42eb63c1fe43f237204770abc34856902
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-02 17:46:59 +0200

    Found a way to get an HTML markup from RenderFragment

---
+++ PDFiller/PDFIller.RazorCodeGeneration.Tests/RazorCodeGenerationTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Xunit;
+using Bunit;
+using PDFiller.RazorCodeGeneration.Tests.Utility;
+
+namespace PDFiller.RazorCodeGeneration.Tests
+{
+    public class RazorCodeGenerationTests
+    {
+        private readonly TestContext _testContext;
+
+        private readonly string _parameterName = "Component";
+
+        public RazorCodeGenerationTests()
+        {
+            _testContext = new TestContext();
+        }
+
+        [Fact]
+        public void GetHtmlFromBlazorComponent()
+        {
+            RenderFragment component = builder =>
+            {
+                builder.OpenElement(0, "div");
+                builder.AddContent(1, "Generated component");
+                builder.CloseElement();
+            };
+
+            var cut = _testContext.RenderComponent<DelegatedComponent>((_parameterName, component));
+            var markup = cut.Markup;
+
+            Assert.Equal("<div>Generated component</div>", markup);
+        }
+    }
+}
