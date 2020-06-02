b639328089f58b9a4769a385c70847beef96a9ea
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-02 22:30:10 +0200

    Adding a funcationality to locate the HtmlForm file

---
+++ PDFiller/PDFIller.RazorCodeGeneration.Tests/HtmlFormFinderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using PDFiller.RazorCodeGeneration;
+using Xunit;
+
+namespace PDFIller.RazorCodeGeneration.Tests
+{
+    public class HtmlFormFinderTests
+    {
+        [Fact]
+        public void ApplicationFolderEmptyString_ShouldThrow()
+        {
+            Action func = () => new HtmlFormFinder(string.Empty);
+
+            var exception = Assert.Throws<ArgumentException>(func);
+
+            Assert.Equal("Expecting a path to the folder of the executable.", exception.Message);
+        }
+
+        [Fact]
+        public void ApplicationFolderIsNotNestedAsExpected_ShouldThrow()
+        {
+            
+            var applicationFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory)?.Parent;
+            Action func = () => new HtmlFormFinder(applicationFolder.FullName);
+
+            var exception = Assert.Throws<ArgumentException>(func);
+
+            Assert.Equal("Unexpected folder structure", exception.Message);
+        }
+
+        [Fact]
+        public void CanReachHtmlFormFile()
+        {
+            var htmlFormFinder = new HtmlFormFinder(AppDomain.CurrentDomain.BaseDirectory);
+
+            var pathToHtmlForm = htmlFormFinder.PathToHtmlForm;
+
+            Assert.True(File.Exists(pathToHtmlForm));
+        }
+    }
+}
---
+++ PDFiller/PDFiller.RazorCodeGeneration/HtmlFormFinder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mime;
+using System.Text;
+
+namespace PDFiller.RazorCodeGeneration
+{
+    public class HtmlFormFinder
+    {
+        private readonly string _solutionFolder = "PDFiller";
+        private readonly string _relativePathToHtmlForm = @"PDFiller\Pages\HtmlForm.razor";
+      
+        public string PathToHtmlForm { get; }
+
+        public HtmlFormFinder(string applicationFolder)
+        {
+            if (string.IsNullOrEmpty(applicationFolder))
+            {
+                throw new ArgumentException("Expecting a path to the folder of the executable.");
+            }
+
+            var dir = new DirectoryInfo(applicationFolder);
+
+            //Expecting something like ...[SOLUTION]\[PROJECT]\bin\Debug\netcoreapp3.1\
+            var solutionRoot = dir?.Parent?.Parent?.Parent?.Parent;
+            if (solutionRoot == null || solutionRoot.Name != _solutionFolder)
+            {
+                throw new ArgumentException("Unexpected folder structure.");
+            }
+
+            PathToHtmlForm = Path.Join(solutionRoot.FullName, _relativePathToHtmlForm);
+        }
+    }
+}
