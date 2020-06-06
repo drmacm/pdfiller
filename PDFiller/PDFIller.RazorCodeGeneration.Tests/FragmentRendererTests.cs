603a8dbb223731485fa10528a07d1a2e10676235
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-06 13:51:44 +0200

    Adding FragmentRenderer

---
+++ PDFiller/PDFIller.RazorCodeGeneration.Tests/FragmentRendererTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Xunit;
+
+namespace PDFiller.RazorCodeGeneration.Tests
+{
+    public class FragmentRendererTests
+    {
+        [Fact]
+        public void RenderFragmentNull_ShouldThrow()
+        {
+            var fragmentRenderer = new FragmentRenderer();
+
+            Action action = () => fragmentRenderer.Render(null);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+
+            Assert.Equal("Fragment to render can't be null.", exception.Message);
+        }
+
+        [Fact]
+        public void ParagraphRenderFragment_ShouldReturnHTML()
+        {
+            var fragmentRenderer = new FragmentRenderer();
+            
+            static void Paragraph(RenderTreeBuilder builder)
+            {
+                builder.OpenElement(0, "p");
+                builder.CloseElement();
+            }
+
+            var result = fragmentRenderer.Render(Paragraph);
+
+            Assert.Equal("<p></p>", result);
+        }
+    }
+}
---
+++ PDFiller/PDFiller.RazorCodeGeneration/FragmentRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using PDFiller.RazorCodeGeneration.Utility;
+
+namespace PDFiller.RazorCodeGeneration
+{
+    public interface IFragmentRenderer
+    {
+        string Render(RenderFragment renderFragment);
+    }
+
+    public class FragmentRenderer : IFragmentRenderer
+    {
+        private readonly TestContext _testContext;
+
+        public FragmentRenderer()
+        {
+            _testContext = new TestContext();
+        }
+
+        public string Render(RenderFragment renderFragment)
+        {
+            if (renderFragment == null)
+            {
+                throw new ArgumentException("Fragment to render can't be null.");
+            }
+
+            var parameter = (DelegatedComponent.ComponentPropertyName, renderFragment);
+            var renderedComponent = _testContext.RenderComponent<DelegatedComponent>(parameter);
+
+            return renderedComponent.Markup;
+        }
+    }
+}
