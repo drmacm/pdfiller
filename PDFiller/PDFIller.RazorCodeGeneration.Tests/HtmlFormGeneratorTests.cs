cb358c93f612b8fd28de06870a3259858a2e5c3f
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-06 14:16:56 +0200

    Adding generation of form fields

---
+++ PDFiller/PDFIller.RazorCodeGeneration.Tests/HtmlFormGeneratorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using PDFiller.Domain;
+using Xunit;
+
+namespace PDFiller.RazorCodeGeneration.Tests
+{
+    public class HtmlFormGeneratorTests
+    {
+        [Fact]
+        public void GenerateForm_FieldListNull_ShouldThrow()
+        {
+            var htmlFormGenerator = new HtmlFormGenerator(new FragmentRenderer());
+            Action action = () => htmlFormGenerator.GenerateForm(null);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+
+            Assert.Equal("List of form fields can't be null.", exception.Message);
+        }
+
+        [Fact]
+        public void GenerateForm_FieldListEmpty_ShouldReturnEmptyString()
+        {
+            var htmlFormGenerator = new HtmlFormGenerator(new FragmentRenderer());
+
+            var formHtml = htmlFormGenerator.GenerateForm(new List<FormField>());
+
+            Assert.Equal(string.Empty, formHtml);
+        }
+
+        [Fact]
+        public void GenerateForm_SingleTextField_ShouldReturnHtmlInputElement()
+        {
+            var htmlFormGenerator = new HtmlFormGenerator(new FragmentRenderer());
+            var formField = new FormField("foo", FormFieldType.TextBox);
+
+            var formHtml = htmlFormGenerator.GenerateForm(new List<FormField> { formField });
+
+            Assert.Equal($@"<label for=""foo"">foo</label>{Environment.NewLine}<input id=""foo"" name=""foo"" required=""required"" />", formHtml);
+        }
+    }
+}
---
+++ PDFiller/PDFiller.RazorCodeGeneration/HtmlFormGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using PDFiller.Domain;
+using PDFiller.RazorCodeGeneration.Utility;
+
+namespace PDFiller.RazorCodeGeneration
+{
+    public class HtmlFormGenerator
+    {
+        private readonly IFragmentRenderer _fragmentRenderer;
+
+        public HtmlFormGenerator(IFragmentRenderer fragmentRenderer)
+        {
+            _fragmentRenderer = fragmentRenderer;
+        }
+
+        public string GenerateForm(List<FormField> formFields)
+        {
+            if (formFields == null)
+            {
+                throw new ArgumentException("List of form fields can't be null.");
+            }
+
+            if (!formFields.Any())
+            {
+                return string.Empty;
+            }
+
+            return RenderTextInputWithLabel(formFields[0]);
+        }
+
+        private string RenderTextInputWithLabel(FormField formField)
+        {
+            var labelMarkup = RenderLabel(formField);
+            var textInputMarkup = RenderTextInput(formField);
+
+            var markup = string.Join(Environment.NewLine, labelMarkup, textInputMarkup);
+
+            return markup;
+        }
+
+        private string RenderLabel(FormField formField)
+        {
+            void Label(RenderTreeBuilder builder)
+            {
+                builder.OpenElement(0, "label");
+                builder.AddAttribute(0, "for", formField.FieldName);
+                builder.AddContent(0, formField.FieldName);
+                builder.CloseElement();
+            }
+
+            return _fragmentRenderer.Render(Label);
+        }
+
+        private string RenderTextInput(FormField formField)
+        {
+            void TextInput(RenderTreeBuilder builder)
+            {
+                builder.OpenElement(0, "input");
+                builder.AddAttribute(0, "id", formField.FieldName);
+                builder.AddAttribute(0, "name", formField.FieldName);
+                builder.AddAttribute(0, "required", "required");
+                builder.CloseElement();
+            }
+
+            return _fragmentRenderer.Render(TextInput);
+        }
+    }
+}
