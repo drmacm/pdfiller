07ceffd617bcd17cbdfb00d84bc4860e80a7139e
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-06 07:08:59 +0200

    Adding code to find the location where PDF form should be placed

---
+++ PDFiller/PDFiller.FileManipulation.Tests/PdfFormFinderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace PDFiller.FileManipulation.Tests
+{
+    public class PdfFormFinderTests
+    {
+        [Fact]
+        public void ApplicationFolderEmptyString_ShouldThrow()
+        {
+            Action action = () => new PdfFormFinder(string.Empty);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+            Assert.Equal("Expecting a path to the folder of the executable.", exception.Message);
+        }
+
+        [Fact]
+        public void ApplicationFolderIsNotNestedAsExpected_ShouldThrow()
+        {
+            var applicationFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory)?.Parent;
+            var pdfFormFinder = new PdfFormFinder(applicationFolder.FullName);
+
+            Action action = () => pdfFormFinder.GetPathToPdfForm();
+
+            var exception = Assert.Throws<ArgumentException>(action);
+            Assert.Equal("Unexpected folder structure.", exception.Message);
+        }
+
+        [Fact]
+        public void ApplicationFolderIsAsExpected_ShouldReturnCorrectFolder()
+        {
+            var applicationFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            var pdfFormFinder = new PdfFormFinder(applicationFolder.FullName);
+
+            var result = pdfFormFinder.GetPathToPdfForm();
+
+            Assert.EndsWith(@"PDFiller\wwwroot\sample-data\src.pdf", result);
+        }
+    }
+}
---
+++ PDFiller/PDFiller.FileManipulation/PdfFormFinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace PDFiller.FileManipulation
+{
+    public class PdfFormFinder
+    {
+        private readonly string _solutionFolder = "PDFiller";
+        private readonly string _relativePathToPdfForm = @"PDFiller\wwwroot\sample-data\src.pdf";
+      
+        public string PathToPdfForm { get; }
+
+        public PdfFormFinder(string applicationFolder)
+        {
+            if (string.IsNullOrEmpty(applicationFolder))
+            {
+                throw new ArgumentException("Expecting a path to the folder of the executable.");
+            }
+
+            var dir = new DirectoryInfo(applicationFolder);
+
+            //Expecting something like ...[SOLUTION]\[PROJECT]\bin\Debug\netcoreapp3.1\
+            var solutionRoot = dir?.Parent?.Parent?.Parent?.Parent;
+            if (solutionRoot == null || solutionRoot.Name != _solutionFolder)
+            {
+                throw new ArgumentException("Unexpected folder structure.");
+            }
+
+            PathToPdfForm = Path.Combine(solutionRoot.FullName, _relativePathToPdfForm);
+        }
+    }
+}
