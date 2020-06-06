200428f8ffdafd8c24646ae261c3b5f8c683e505
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-06 13:37:29 +0200

    Adding the DelegatedComponent to the project

---
+++ PDFiller/PDFiller.RazorCodeGeneration/Utility/DelegatedComponent.cs
@@ -0,0 +1,26 @@
+﻿using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace PDFiller.RazorCodeGeneration.Utility
+{
+    /// <summary>
+    /// Component used to generate HTML markup from the <see cref="RenderFragment"/>,
+    /// with the help of <see cref="Bunit"/> library and <see cref="TestContext"/>.
+    /// </summary>
+    public class DelegatedComponent : ComponentBase
+    {
+        /// <summary>
+        /// We need the name of the <see cref="Component"/> property to be able
+        /// to pass the actual value of <see cref="RenderFragment"/> to be rendered.
+        /// </summary>
+        public static string ComponentPropertyName = nameof(Component);
+
+        /// <summary>
+        /// Represents the HTML fragment that will be rendered.
+        /// </summary>
+        [Parameter] public RenderFragment Component { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder) => Component(builder);
+    }
+}
