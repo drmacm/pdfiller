85e971d15aaa2a5ba5927c313db2a5c1a511217b
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-06 15:23:09 +0200

    Testing all the pieces together

--- PDFiller/PDFiller.Runner/Program.cs
+++ PDFiller/PDFiller.Runner/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using PDFiller.PDFManipulation;
+using System;
+using System.IO;
+using PDFiller.Domain;
+using PDFiller.RazorCodeGeneration;
 
 namespace PDFiller.Runner
 {
@@ -6,6 +10,34 @@ namespace PDFiller.Runner
     {
         static void Main(string[] args)
         {
+            CopyPdfFormToWebsite();
+        }
+
+        private static void CopyPdfFormToWebsite()
+        {
+            Console.WriteLine("Enter a path to the PDF form:");
+            var pdfFormSource = Console.ReadLine();
+            if (string.IsNullOrEmpty(pdfFormSource))
+            {
+                pdfFormSource = @"SamplePDFs\TextBoxForm.pdf";
+            }
+
+            var applicationFolder = AppDomain.CurrentDomain.BaseDirectory;
+            PdfFormFinder pdfFormFinder = new PdfFormFinder(applicationFolder);
+            var pdfFormDestination = pdfFormFinder.GetPathToPdfForm();
+            
+            FileCopier.CopyPdfFormToBlazorProject(pdfFormSource, pdfFormDestination);
+
+            var formFields = PdfFormLoader.GetFormFields(pdfFormDestination);
+
+            var htmlFormGenerator = new HtmlFormGenerator(new FragmentRenderer());
+            var formMarkup = htmlFormGenerator.GenerateForm(formFields);
+
+            var htmlFormFinder = new HtmlFormFinder(applicationFolder);
+            var pathToHtmlForm = htmlFormFinder.GetPathToHtmlForm();
+            
+            var htmlFormUpdater = new HtmlFormUpdater();
+            htmlFormUpdater.UpdateHtmlFormInBlazorProject(pathToHtmlForm, formMarkup);
         }
     }
 }
