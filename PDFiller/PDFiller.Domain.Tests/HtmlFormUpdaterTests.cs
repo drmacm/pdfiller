e102956f5b202a0984a092bf346f45bad6c54d97
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-06 15:18:09 +0200

    Adding the possibility to update the HTML form with the generated markup

---
+++ PDFiller/PDFiller.Domain.Tests/HtmlFormUpdaterTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace PDFiller.Domain.Tests
+{
+    public class HtmlFormUpdaterTests
+    {
+        [Fact]
+        public void UpdateHtmlFormInBlazorProject_PathToHtmlFormNotProvided_ShouldThrow()
+        {
+            var htmlFormUpdater = new HtmlFormUpdater();
+            Action action = () => htmlFormUpdater.UpdateHtmlFormInBlazorProject(string.Empty, string.Empty);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+
+            Assert.Equal("Path to HTML form not provided.", exception.Message);
+        }
+
+        [Fact]
+        public void CopyPdfFormToBlazorProject_InvalidPathToPdfFormSource_ShouldThrow()
+        {
+            var htmlFormUpdater = new HtmlFormUpdater();
+            Action action = () => htmlFormUpdater.UpdateHtmlFormInBlazorProject(@"C:\IShouldNotExist.razor", string.Empty);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+
+            Assert.Equal("Invalid path to HTML form.", exception.Message);
+        }
+
+        [Fact]
+        public void UpdateHtmlFormInBlazorProject_FormMarkupNotProvided_ShouldThrow()
+        {
+            var htmlFormFinder = new HtmlFormFinder(AppDomain.CurrentDomain.BaseDirectory);
+            var pathToHtmlForm = htmlFormFinder.GetPathToHtmlForm();
+            var htmlFormUpdater = new HtmlFormUpdater();
+            Action action = () => htmlFormUpdater.UpdateHtmlFormInBlazorProject(pathToHtmlForm, string.Empty);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+
+            Assert.Equal("Form markup is null or empty.", exception.Message);
+        }
+
+        [Fact]
+        public void CanUpdateHtmlForm()
+        {
+            var htmlFormFinder = new HtmlFormFinder(AppDomain.CurrentDomain.BaseDirectory);
+            var pathToHtmlForm = htmlFormFinder.GetPathToHtmlForm();
+            var formMarkup = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+
+            var htmlFormUpdater = new HtmlFormUpdater();
+            
+            var originalHtmlFormContent = File.ReadAllText(pathToHtmlForm);
+            Assert.DoesNotContain(formMarkup, originalHtmlFormContent);
+
+            htmlFormUpdater.UpdateHtmlFormInBlazorProject(pathToHtmlForm, formMarkup);
+            var newHtmlFormContent = File.ReadAllText(pathToHtmlForm);
+            Assert.Contains(formMarkup, newHtmlFormContent);
+
+            //revert
+            File.WriteAllText(pathToHtmlForm, originalHtmlFormContent);
+            var cleanedUpHtmlFormContent = File.ReadAllText(pathToHtmlForm);
+            Assert.DoesNotContain(formMarkup, cleanedUpHtmlFormContent);
+        }
+    }
+}
---
+++ PDFiller/PDFiller.Domain/HtmlFormUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace PDFiller.Domain
+{
+    public class HtmlFormUpdater
+    {
+        private readonly string _placeholder = "<!--[FORM_FIELDS_PLACEHOLDER]-->";
+        
+        public void UpdateHtmlFormInBlazorProject(string pathToHtmlForm, string formMarkup)
+        {
+            if (string.IsNullOrEmpty(pathToHtmlForm))
+            {
+                throw new ArgumentException("Path to HTML form not provided.");
+            }
+            if (!File.Exists(pathToHtmlForm))
+            {
+                throw new ArgumentException("Invalid path to HTML form.");
+            }
+
+            if (string.IsNullOrEmpty(formMarkup))
+            {
+                throw new ArgumentException("Form markup is null or empty.");
+            }
+
+            var existingFormMarkup = File.ReadAllText(pathToHtmlForm);
+
+            var updatedFormMarkup = existingFormMarkup.Replace(_placeholder, formMarkup);
+
+            File.WriteAllText(pathToHtmlForm, updatedFormMarkup);
+        }
+    }
+}
