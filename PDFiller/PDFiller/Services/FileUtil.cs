386fe7140914adb2c2d84899ac38bd50c7708971
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-07 08:03:09 +0200

    Adding functionality to fill the PDF form based on the value on the HTML form. HTML form fields and the model are hardcoded

---
+++ PDFiller/PDFiller/Services/FileUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace PDFiller.Services
+{
+    public static class FileUtil
+    {
+        public static ValueTask<object> SaveAs(
+            this IJSRuntime js, 
+            string filename, 
+            byte[] data)
+        {
+            return js.InvokeAsync<object>(
+                "saveAsFile", 
+                filename, 
+                Convert.ToBase64String(data));
+        }
+    }
+}
---
+++ PDFiller/PDFiller/Services/PdfFormFillingService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+using iText.Layout;
+
+namespace PDFiller.Services
+{
+    public class PdfFormFillingService
+    {
+        private IDictionary<string, PdfFormField> _formFields;
+
+        public MemoryStream FillForm(MemoryStream formToFill, FormModel model)
+        {
+            var writerStream = new MemoryStream();
+
+            var pdfReader = new PdfReader(formToFill);
+            var pdfWriter = new PdfWriter(writerStream);
+            var pdfDocument = new PdfDocument(pdfReader, pdfWriter);
+            var document = new Document(pdfDocument);
+            var form = PdfAcroForm.GetAcroForm(pdfDocument, true);
+
+            _formFields = form.GetFormFields();
+            FillFormFields(model);
+
+            document.Close();
+
+            return writerStream;
+        }
+
+        private void FillFormFields(FormModel model)
+        {
+            FillFormField("name", model.Name);
+        }
+
+        private void FillFormField(string fieldName, string value)
+        {
+            if (_formFields.ContainsKey(fieldName))
+            {
+                var formField = _formFields[fieldName];
+                formField.SetValue(value);
+            }
+        }
+    }
+}
