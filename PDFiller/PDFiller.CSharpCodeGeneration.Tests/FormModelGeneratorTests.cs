d6d6fef762afca8fb1562f540e566c0154cf272e
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-09 20:39:41 +0200

    Improving FormModelGenerator

---
+++ PDFiller/PDFiller.CSharpCodeGeneration.Tests/FormModelGeneratorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using PDFiller.Domain;
+using Xunit;
+
+namespace PDFiller.CSharpCodeGeneration.Tests
+{
+    public class FormModelGeneratorTests
+    {
+        private readonly FormModelGenerator _formModelGenerator;
+
+        public FormModelGeneratorTests()
+        {
+            _formModelGenerator = new FormModelGenerator();
+        }
+
+        [Fact]
+        public void Generate_PathToFormModelEmpty_ShouldThrow()
+        {
+            Action action = () => _formModelGenerator.Generate(new List<FormField>(), string.Empty);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+
+            Assert.Equal("Path to FormModel class not provided.", exception.Message);
+        }
+
+        [Fact]
+        public void Generate_InvalidPathToFormModel_ShouldThrow()
+        {
+            Action action = () => _formModelGenerator.Generate(new List<FormField>(), @"C:\IShouldNotExist.cs");
+
+            var exception = Assert.Throws<ArgumentException>(action);
+
+            Assert.Equal("Invalid path to FormModel class.", exception.Message);
+        }
+
+        [Fact]
+        public void CanGenerateFormModel()
+        {
+            var pathToFormModel = @"SampleCodeFiles\SampleFormModel.cs";
+            var expectedSource = "public string Baz { get; set; }";
+
+            var originalFormModelContent = File.ReadAllText(pathToFormModel);
+            Assert.DoesNotContain(expectedSource, originalFormModelContent);
+
+            var formFields = new List<FormField>
+            {
+                new FormField("Baz", FormFieldType.TextBox)
+            };
+            
+            var formModelGenerator = new FormModelGenerator();
+            var newFormModelContent = formModelGenerator.Generate(formFields, pathToFormModel);
+
+            Assert.Contains(expectedSource, newFormModelContent);
+        }
+    }
+}
--- PDFiller/PDFiller.CSharpCodeGeneration/FormModelGenerator.cs
+++ PDFiller/PDFiller.CSharpCodeGeneration/FormModelGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.CodeAnalysis.CSharp;
 using PDFiller.Domain;
@@ -9,6 +10,15 @@ namespace PDFiller.CSharpCodeGeneration
     {
         public string Generate(List<FormField> formFields, string pathToFormModel)
         {
+            if (string.IsNullOrEmpty(pathToFormModel))
+            {
+                throw new ArgumentException("Path to FormModel class not provided.");
+            }
+            if (!File.Exists(pathToFormModel))
+            {
+                throw new ArgumentException("Invalid path to FormModel class.");
+            }
+
             var formModelPropertyGenerator = new FormFieldPropertyGenerator(formFields);
 
             var formModelContent = File.ReadAllText(pathToFormModel);
