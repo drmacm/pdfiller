5734520195a47a37421560fa26ac93478944338a
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-10 20:53:29 +0200

    Adding an option to populate values for all string properties

--- PDFiller/PDFiller.Website/Models/FormModel.cs
+++ PDFiller/PDFiller.Website/Models/FormModel.cs
@@ -4,5 +4,18 @@ namespace PDFiller.Website.Models
 {
     public class FormModel
     {
+        public FormModel(bool setValues = true)
+        {
+            if (setValues)
+            {
+                foreach (var prop in GetType().GetProperties())
+                {
+                    if (prop.PropertyType == typeof(string))
+                    {
+                        prop.SetValue(this, prop.Name, null);
+                    }
+                }
+            }
+        }
     }
 }
