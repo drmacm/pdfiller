cf1eea735801f036d79ddc63bb904267bca23aaf
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-10 19:39:08 +0200

    Tweaking the HtmlForm and FormModel

--- PDFiller/PDFiller.Website/Models/FormModel.cs
+++ PDFiller/PDFiller.Website/Models/FormModel.cs
@@ -4,10 +4,5 @@ namespace PDFiller.Website.Models
 {
     public class FormModel
     {
-        [Required]
-        [StringLength(10, MinimumLength = 6, ErrorMessage = "Name is too short or too long.")]
-        public string Name { get; set; }
-       
-        public bool Bar { get; set; }
     }
 }
