bdbeb0b5d7635454a77356d1586d2603e3ba7866
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-12 08:11:54 +0200

    Coping the LICENSE file to the generated website

--- PDFiller/scripts/Copy-GeneratedWebsite.ps1
+++ PDFiller/scripts/Copy-GeneratedWebsite.ps1
@@ -1,5 +1,5 @@
 Write-Host "Copying a solution with generated website to a separate folder."
-$GeneratedWebsiteFolder = Read-Host -Prompt "Enter a folder name: "
+$GeneratedWebsiteFolder = Read-Host -Prompt "Enter a folder name for your static website"
 
 Copy-Item "..\PDFiller.Domain" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Domain" -Recurse
 Write-Host "Successfully copied the PDFiller.Domain project."
@@ -9,6 +9,9 @@ Write-Host "Successfully copied the PDFiller.Website project."
 
 Copy-Item "..\PDFiller.Website.sln" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Website.sln"
 Write-Host "Successfully copied the solution file."
+
+Copy-Item "..\..\LICENSE" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\LICENSE"
+Write-Host "Successfully copied the license file."
 
 Write-Host "Generated website is available in ""..\GeneratedWebsites\$GeneratedWebsiteFolder""."
 
