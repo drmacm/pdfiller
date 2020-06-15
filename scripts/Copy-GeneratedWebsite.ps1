72c329ff512904aa02388a5573f10f1809c0886c
Mladen Drmac <drmac.mladen@gmail.com>
2020-06-15 07:02:59 +0200

    Adding a -Force parameter to the website copying script

--- scripts/Copy-GeneratedWebsite.ps1
+++ scripts/Copy-GeneratedWebsite.ps1
@@ -1,16 +1,17 @@
 Write-Host "Copying a solution with generated website to a separate folder."
+Write-Host "Warning: Existing files in the folder will be overwritten!"
 $GeneratedWebsiteFolder = Read-Host -Prompt "Enter a folder name for your static website"
 
-Copy-Item "..\PDFiller.Models" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Models" -Recurse
+Copy-Item "..\PDFiller.Models" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Models" -Recurse -Force
 Write-Host "Successfully copied the PDFiller.Models project."
 
-Copy-Item "..\PDFiller.Website" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Website" -Recurse
+Copy-Item "..\PDFiller.Website" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Website" -Recurse -Force
 Write-Host "Successfully copied the PDFiller.Website project."
 
-Copy-Item "..\PDFiller.Website.sln" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Website.sln"
+Copy-Item "..\PDFiller.Website.sln" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\PDFiller.Website.sln" -Force
 Write-Host "Successfully copied the solution file."
 
-Copy-Item "..\LICENSE" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\LICENSE"
+Copy-Item "..\LICENSE" -Destination "..\GeneratedWebsites\$GeneratedWebsiteFolder\LICENSE" -Force
 Write-Host "Successfully copied the license file."
 
 Write-Host "Generated website is available in ""..\GeneratedWebsites\$GeneratedWebsiteFolder""."
